432a33a027ab7406e93baca193e0cbf2cdc89ff4
egaotan <chain_dev@163.com>
2022-08-24 09:41:25 +0800

    update

--- faucet/client.go
+++ faucet/client.go
@@ -1,7 +1,7 @@
 package faucet
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"github.com/motoko9/aptos-go/rpc"
 )
@@ -11,15 +11,12 @@ type FundAccountResult struct {
 
 func FundAccount(address string, amount uint64) ([]string, error) {
 	client := rpc.New("https://faucet.devnet.aptoslabs.com")
-	result, code, err := client.Post("/mint", map[string]string{
+	var hashes []string
+	code, err := client.Post(context.Background(), "/mint", map[string]string{
 		"amount":  fmt.Sprintf("%d", amount),
 		"address": address,
-	}, nil)
+	}, nil, &hashes)
 	if err != nil || code != 200 {
-		return nil, err
-	}
-	var hashes []string
-	if err = json.Unmarshal(result, &hashes); err != nil {
 		return nil, err
 	}
 	return hashes, nil
--- rpc/account.go
+++ rpc/account.go
@@ -2,6 +2,8 @@ package rpc
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 )
 
 type Account struct {
@@ -16,4 +18,42 @@ func (cl *Client) Account(ctx context.Context, address string) (*Account, error)
 		return nil, err
 	}
 	return &account, nil
+}
+
+type AccountResource struct {
+	// only support CoinStore type
+	// todo
+	T    string    `json:"type"`
+	//Data interface{} `json:"data"`
+	Data json.RawMessage `json:"data"`
+}
+
+type AccountResources []AccountResource
+
+func (cl *Client) AccountResources(ctx context.Context, address string, version uint64) (*AccountResources, error) {
+	var params map[string]string
+	if version != 0 {
+		params = make(map[string]string)
+		params["version"] = fmt.Sprintf("%d", version)
+	}
+	var accountResources AccountResources
+	code, err := cl.Get(ctx, "/accounts/"+address+"/resources", params, &accountResources)
+	if err != nil || code != 200 {
+		return nil, err
+	}
+	return &accountResources, nil
+}
+
+func (cl *Client) AccountResourceByAddressAndType(ctx context.Context, address string, t string, version uint64) (*AccountResource, error) {
+	var params map[string]string
+	if version != 0 {
+		params = make(map[string]string)
+		params["version"] = fmt.Sprintf("%d", version)
+	}
+	var accountResource AccountResource
+	code, err := cl.Get(ctx, "/accounts/"+address+"/resource/"+t, params, &accountResource)
+	if err != nil || code != 200 {
+		return nil, err
+	}
+	return &accountResource, nil
 }
