dd6531cff43b9c2f21766a15a0b13b2e8e136623
egaotan <chain_dev@163.com>
2022-08-27 19:24:27 +0800

    update

--- base-example/transfer_test.go
+++ base-example/transfer_test.go
@@ -5,11 +5,89 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/motoko9/aptos-go/aptos"
+	"github.com/motoko9/aptos-go/faucet"
 	"github.com/motoko9/aptos-go/rpc"
 	"github.com/motoko9/aptos-go/rpcmodule"
 	"github.com/motoko9/aptos-go/wallet"
 	"testing"
+	"time"
 )
+
+func TestNewToAccount(t *testing.T) {
+	ctx := context.Background()
+
+	// new account
+	wallet := wallet.New()
+	wallet.Save("account_to")
+	address := wallet.Address()
+	fmt.Printf("address: %s\n", address)
+
+	// fund (max: 20000)
+	amount := uint64(20000)
+	hashes, err := faucet.FundAccount(address, amount)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("fund txs: %v\n", hashes)
+
+	//
+	time.Sleep(time.Second * 5)
+
+	// new rpc
+	client := aptos.New(rpc.DevNet_RPC)
+
+	// latest ledger
+	ledger, err := client.Ledger(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// check account
+	balance, err := client.AccountBalance(ctx, address, aptos.AptosCoin, ledger.LedgerVersion)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("account balance: %d\n", balance)
+}
+
+func TestReadToAccount(t *testing.T) {
+	ctx := context.Background()
+
+	// new account
+	wallet, err := wallet.NewFromKeygenFile("account_to")
+	if err != nil {
+		panic(err)
+	}
+	address := wallet.Address()
+	fmt.Printf("address: %s\n", address)
+
+	// fund (max: 20000)
+	amount := uint64(20000)
+	hashes, err := faucet.FundAccount(address, amount)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("fund txs: %v\n", hashes)
+
+	//
+	time.Sleep(time.Second * 5)
+
+	// new rpc
+	client := aptos.New(rpc.DevNet_RPC)
+
+	// latest ledger
+	ledger, err := client.Ledger(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// check account
+	balance, err := client.AccountBalance(ctx, address, aptos.AptosCoin, ledger.LedgerVersion)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("account balance: %d\n", balance)
+}
 
 func TestTransfer_raw(t *testing.T) {
 	ctx := context.Background()
