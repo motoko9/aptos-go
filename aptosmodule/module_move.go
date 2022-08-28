04ae4ef3b2259e6de758502b2d7f2cfb00d45308
egaotan <chain_dev@163.com>
2022-08-28 14:04:17 +0800

    update

--- aptosmodule/module_move.go
+++ aptosmodule/module_move.go
@@ -6,26 +6,35 @@ type Guid struct {
 		CreationNum uint64 `json:"creation_num,string"`
 	} `json:"id"`
 }
-type Coin struct {
-	Value uint64 `json:"value,string"`
-}
 
-type CoinEvents struct {
+type Events struct {
 	Counter uint64 `json:"counter,string"`
 	Guid    Guid   `json:"guid"`
 }
 
+type Coin struct {
+	Value uint64 `json:"value,string"`
+}
+
 type CoinStore struct {
-	Coin           Coin       `json:"coin"`
-	DepositEvents  CoinEvents `json:"deposit_events"`
-	WithdrawEvents CoinEvents `json:"withdraw_events"`
+	Coin           Coin   `json:"coin"`
+	DepositEvents  Events `json:"deposit_events"`
+	WithdrawEvents Events `json:"withdraw_events"`
 }
 
 type CoinInfo struct {
-	Name string `json:"name"`
-	Symbol string `json:"symbol"`
+	Name     string `json:"name"`
+	Symbol   string `json:"symbol"`
 	Decimals uint64 `json:"decimals"`
 	// todo
 	// supply is not uint64
 	// Supply uint64 `json:"supply"`
+}
+
+type Account struct {
+	AuthenticationKey  string `json:"authentication_key"`
+	CoinRegisterEvents Events `json:"coin_register_events"`
+	GuidCreationNum    uint64 `json:"guid_creation_num,string"`
+	KeyRotationEvents  Events `json:"key_rotation_events"`
+	SequenceNumber     uint64 `json:"sequence_number,string"`
 }
