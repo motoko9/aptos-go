a257a29d93739dd5737a4f2bbdfd7d2f46afcc04
egaotan <chain_dev@163.com>
2023-04-23 21:34:56 +0800

    update

--- aptos/transaction.go
+++ aptos/transaction.go
@@ -64,3 +64,23 @@ func (cl *Client) SignAndSubmitTransaction(ctx context.Context, sender string, s
 	// submit
 	return cl.SubmitTransaction(ctx, submitReq)
 }
+
+func (cl *Client) ExeSimulateTransaction(ctx context.Context, sender string, sequence uint64, payload *rpcmodule.TransactionPayload) (rpcmodule.SimulateTransactionRsp, *rpcmodule.AptosError) {
+	tx := &rpcmodule.SubmitTransactionRequest{
+		Sender:                  sender,
+		SequenceNumber:          sequence,
+		MaxGasAmount:            uint64(80000),
+		GasUnitPrice:            uint64(100),
+		ExpirationTimestampSecs: uint64(time.Now().Unix() + 600),
+		Payload:                 payload,
+		Signature: rpcmodule.Signature{
+			Type: rpcmodule.Ed25519Signature,
+			Object: rpcmodule.SignatureEd25519Signature{
+				Type:      rpcmodule.Ed25519Signature,
+				PublicKey: "0xd36df53c46ca6c046648a85083482149a6423b06a3e35cd2f91b01d656c06e73",
+				Signature: "0x945073c4b0d389271b1e6959e5238a1d6f6f82aa6dc09ca6cd31eb6952bde2781b6338d2470b6d703d844af58da952ef0635e29b89da4af1d4a98ef690e8990d",
+			},
+		},
+	}
+	return cl.SimulateTransaction(ctx, tx)
+}
